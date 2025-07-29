0eeb7c779b161b481608ee9d1099c3192199db98
chalaa <chalaolanigeleta@gmail.com>
2025-07-29 11:05:14 +0300

    Add manifest file for the module

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Public Attachments Override',
+    'version': '1.0.0',
+    'summary': 'Automatically make all attachments public in Odoo 18.',
+    'description': """
+        This module overrides the default behavior of Odoo 18 to automatically set all attachments as public.
+        Useful for environments where unrestricted access to attachments is required.
+    """,
+    'category': 'Tools',
+    'author': 'Chala Olani',
+    'website': 'https://github.com/chalaa/Odoo-Attachment-Public-Override',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'maintainer': 'Chala Olani',
+    'support': 'chalaolani@gmail.com',
+    
+}
